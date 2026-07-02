d4603d91778a3c8dfe6ba9095dbb480ecc1b919b
maxwase <max.vvase@gmail.com>
2026-07-02 23:33:06 +0300

    Add proxy-formatter

--- examples/custom_format.rs
+++ examples/custom_format.rs
@@ -2,10 +2,14 @@
 //!
 //! Run: `cargo run --example custom_format`
 //!
-//! Output: `Outer -> Middle -> inner`
+//! Output:
+//! ```text
+//! Outer -> Middle -> inner
+//! { "message": "Outer -> Middle -> inner" }
+//! ```
 
 use core::{error::Error, fmt};
-use std::io;
+use std::{io, marker::PhantomData};
 
 use errortools::{Format, FormatError, chain};
 use itertools::Itertools;
@@ -15,6 +19,20 @@ struct Arrow;
 impl<E: Error + ?Sized> Format<E> for Arrow {
     fn fmt(error: &E, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", chain(&error).format(" -> "))
+    }
+}
+
+/// You can also implement your own custom format strategy that uses another format strategy to render the error message.
+/// This is useful for rendering errors in a structured format like JSON or OpenTelemetry.
+struct Json<F>(PhantomData<fn() -> F>);
+
+impl<E, F> Format<E> for Json<F>
+where
+    E: std::error::Error + ?Sized,
+    F: for<'a> Format<&'a E>,
+{
+    fn fmt(error: &E, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{{ \"message\": \"{}\" }}", error.formatted::<F>())
     }
 }
 
@@ -33,4 +51,5 @@ enum MidError {
 fn main() {
     let err = AppError::Outer(MidError::Middle(io::Error::other("inner")));
     println!("{}", err.formatted::<Arrow>());
+    println!("{}", err.formatted::<Json<Arrow>>());
 }
